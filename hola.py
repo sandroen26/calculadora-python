457d1441b9b43f2042249f95e426485488af6a0f
Sanen26 <sandrosanabria1990@gmail.com>
2026-09-03 00:11:39 -0300

    primer commit

---
+++ hola.py
@@ -0,0 +1,35 @@
+#print("hola mundo")
+#texto = "luz"
+#num = "12345"
+
+#input("ingrese su nombre: ")
+#print(texto)
+
+print("***__ calculadora tradicional __***")
+
+numero1 = int(input("ingrse primer numero "))
+operacion = input("elija que operacion desea: (+ - * / %) ")
+numero2 = int(input("ingrse segundo numero "))
+
+if operacion == "+":
+    resultado = numero1 +  numero2
+    
+elif operacion == "-":
+    resultado = numero1 -  numero2
+    
+elif operacion == "*":
+    resultado = numero1 *  numero2
+
+elif operacion == "/":
+    resultado = numero1 //  numero2
+
+elif operacion == "%":
+    resultado = numero1 %  numero2
+
+
+if resultado < 0:
+    resultado = -resultado
+else:
+    resultado
+
+print("hello world")
