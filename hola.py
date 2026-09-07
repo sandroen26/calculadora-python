2fc13f0556933ee9468f560a44ff1518fc641624
Sanen26 <sandrosanabria1990@gmail.com>
2026-09-07 01:00:32 -0300

    se agrego comentarios en puntos especificos

--- hola.py
+++ hola.py
@@ -5,31 +5,37 @@
 #input("ingrese su nombre: ")
 #print(texto)
 
+#titulo
 print("***__ calculadora tradicional __***")
 
+#pedimos los datos al usuario
 numero1 = int(input("ingrse primer numero "))
 operacion = input("elija que operacion desea: (+ - * / %) ")
 numero2 = int(input("ingrse segundo numero "))
 
+#ejecutamos la operacion que selecciono el usuario
+#suma
 if operacion == "+":
     resultado = numero1 +  numero2
-    
+#resta    
 elif operacion == "-":
     resultado = numero1 -  numero2
-    
+#multiplicacion    
 elif operacion == "*":
     resultado = numero1 *  numero2
-
+#division
 elif operacion == "/":
     resultado = numero1 //  numero2
-
+#resto
 elif operacion == "%":
     resultado = numero1 %  numero2
 
-
+#en caso de que el resultado sea negativo, con esto lo revertimos haciendolo positivo.
 if resultado < 0:
     resultado = -resultado
 else:
     resultado
 
-print("hello world")
+#imprimimos el resultado.
+print(resultado)
+print(type(resultado))
